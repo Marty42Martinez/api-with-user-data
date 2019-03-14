51a66684c663d073db38223ff2f3accd31258a2e
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-03-13 18:50:12 -0700

    creating header component functions

--- src/shared/header-component.js
+++ src/shared/header-component.js
@@ -1,3 +1,5 @@
+import { auth } from '../firebase.js';
+
 export function makeProfileDisplay(user) {
    const html = `
       <span class="profile-display">
@@ -9,4 +11,36 @@ export function makeProfileDisplay(user) {
    const template = document.createElement('template');
    template.innerHTML = html;
    return template.content;
+}
+
+function makeStaticHeader() {
+   const html = `
+      <span class="header-container">
+         <img src="./assets/schwifty-mock-03.jpg" alt="Schwifty Art of Rick and Morty">
+         <h1>Rick and Morty</h1>
+      </span>
+    `;
+
+   const template = document.createElement('template');
+   template.innerHTML = html;
+   return template.content;
+}
+
+const headerNode = document.getElementById('header');
+
+export default function loadHeader(options) {
+
+   const staticHeader = makeStaticHeader();
+   if(options && options.skipAuth) {
+      headerNode.appendChild(staticHeader);
+      return;
+   }
+
+   const fullHeaderNode = staticHeader.querySelector('.header-container');
+
+   auth.onAuthStateChanged(user => {
+      
+   });
+
+
 }
