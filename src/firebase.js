6abac9aeedc476c9b4485ab837567d824be6be44
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-03-13 17:30:39 -0700

    set up firebase project, added important links

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+
+
+var config = {
+   apiKey: 'AIzaSyBcTjf8rq1TXKUnppvuMaBGg0mKxveJNds',
+   authDomain: 'rick-morty-favorites.firebaseapp.com',
+   databaseURL: 'https://rick-morty-favorites.firebaseio.com',
+   projectId: 'rick-morty-favorites'
+};
+
+firebase.initializeApp(config);
